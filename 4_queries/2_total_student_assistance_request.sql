aed3fb9f9eaa079591542eff3f967b280d48aae8
megan <meganambermcbride@gmail.com>
2022-06-28 20:15:52 -0400

    Add total assistance requests for student

--- 4_queries/2_total_student_assistance_request.sql
+++ 4_queries/2_total_student_assistance_request.sql
@@ -1,4 +1,4 @@
-SELECT count(assistance_requests), students.name
+SELECT count(assistance_requests.*) as total_assistances, students.name
 FROM assistance_requests
 JOIN students ON students.id = student_id
 WHERE name = 'Elliot Dickinson'
